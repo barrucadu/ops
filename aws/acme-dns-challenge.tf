0a37c655f254a2bc0f149321358bca93acc048b2
Michael Walker <mike@barrucadu.co.uk>
2024-12-10 13:50:05 +0000

    Create a user with limited access for ACME DNS-01 challenge
    This is so I can potentially migrate from HTTP-01 and geo-distribute my
    website for fun.

---
+++ aws/acme-dns-challenge.tf
@@ -0,0 +1,84 @@
+locals {
+  # zones are created through octodns, not terraform
+  hosted_zones = {
+    barrucadu_co_uk = {
+      domain = "barrucadu.co.uk"
+      zone_id = "ZNU9LQOCWHL2G"
+    }
+  }
+}
+
+resource "aws_iam_user" "acme_dns_challenge" {
+  name = "acme-dns-challenge"
+}
+
+resource "aws_iam_user_policy_attachment" "acme_dns_challenge" {
+  for_each = local.hosted_zones
+
+  user       = aws_iam_user.acme_dns_challenge[each.key].name
+  policy_arn = aws_iam_policy.acme_dns_challenge[each.key].arn
+}
+
+/* ************************************************************************* */
+
+resource "aws_iam_policy" "acme_dns_challenge" {
+  for_each = local.hosted_zones
+
+  name        = "acme-dns-challenge-${each.value.domain}"
+  description = "Restricted access to the zone for ${each.value.domain}."
+  policy      = data.aws_iam_policy_document.acme_dns_challenge[each.key].json
+}
+
+// https://go-acme.github.io/lego/dns/route53/index.html#least-privilege-policy-for-production-purposes
+data "aws_iam_policy_document" "acme_dns_challenge" {
+  for_each = local.hosted_zones
+
+  statement {
+    actions = [
+      "route53:GetChange",
+    ]
+
+    resources = [
+      "arn:aws:route53:::change/*",
+    ]
+  }
+  statement {
+    actions = [
+      "route53:ListHostedZonesByName",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+  statement {
+    actions = [
+      "route53:ListResourceRecordSets",
+    ]
+
+    resources = [
+      "arn:aws:route53:::hostedzone/${each.value.zone_id}",
+    ]
+  }
+  statement {
+    actions = [
+      "route53:ChangeResourceRecordSets",
+    ]
+
+    resources = [
+      "arn:aws:route53:::hostedzone/${each.value.zone_id}",
+    ]
+
+    condition {
+      test = "ForAllValues:StringEquals"
+      variable = "route53:ChangeResourceRecordSetsNormalizedRecordNames"
+      values = ["_acme-challenge.${each.value.domain}"]
+    }
+
+    condition {
+      test = "ForAllValues:StringEquals"
+      variable = "route53:ChangeResourceRecordSetsRecordTypes"
+      values = ["TXT"]
+    }
+  }
+}
